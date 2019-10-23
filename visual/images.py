5831086b1383d196ba3cafbaa3b37bcbe52ec2e2
Ethan Harris <ethanwharris@gmail.com>
2019-10-23 08:52:31 +0100

    Updates

--- visual/images.py
+++ visual/images.py
@@ -328,5 +328,5 @@ class CPPNImage(Image):
         res.height = height
         res.width = width
 
-        res.loc = CPPNImage._make_grid(res.height, res.width).to(res.device)
+        res.loc = CPPNImage._make_grid(res.height, res.width).to(res.loc.device)
         return res
